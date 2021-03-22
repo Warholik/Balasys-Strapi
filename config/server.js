09c7ae27fb51a844a633bc791c37c81d6327d117
Warholik <varholikm00@gmail.com>
2021-03-22 14:46:06 +0100

    changed port

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: 8501,
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'd12787aa2e1be42189dc3c9483e7d047'),
