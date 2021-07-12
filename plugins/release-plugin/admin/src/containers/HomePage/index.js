5e7bc33858dc2d0f6d074cfa7e0c333f1060d988
Káposzta Gergő <gergo.kaposzta@kifli.tech>
2021-07-12 18:10:08 +0200

    Release plugin v2

--- plugins/release-plugin/admin/src/containers/HomePage/index.js
+++ plugins/release-plugin/admin/src/containers/HomePage/index.js
@@ -1,15 +1,32 @@
 import React, {memo} from "react";
 import {Button, Text} from "@buffetjs/core";
 import {request} from "strapi-helper-plugin";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUpload } from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faUpload} from "@fortawesome/free-solid-svg-icons";
 import pluginId from "../../pluginId";
+
+import "./releaseplugin.css";
 
 const HomePage = () => {
 
   const triggerPublish = async () => {
     const result = await request(`/${pluginId}/publish`, {method: "GET"});
-    if(result.success === 200){
+    if (result.success === 200) {
+      strapi.notification.toggle({
+        type: 'success',
+        message: "Sikeres release.",
+      });
+    } else {
+      strapi.notification.toggle({
+        type: 'warning',
+        message: "Sikertelen release.",
+      });
+    }
+  };
+
+  const triggerPublishBlackowl = async () => {
+    const result = await request(`/${pluginId}/publishBlackowl`, {method: "GET"});
+    if (result.success === 200) {
       strapi.notification.toggle({
         type: 'success',
         message: "Sikeres release.",
@@ -23,49 +40,95 @@ const HomePage = () => {
   };
 
   return (
-    <>
-      <div style={{display: "grid", gridTemplateColumns: "1fr 4fr 4fr 1fr", padding: "40px 40px 40px 40px", gap: "40px"}}>
-        <div/>
-        <div style={{
-          justifySelf: "stretch",
-          width: 100 + "%",
-          padding: "20px 20px 20px 20px",
-          boxShadow: "0 2px 4px 0 #e3e9f3",
-          borderRadius: "3px",
-          background: "white"
-        }}>
-          <h1 style={{marginBottom: "20px"}}>Release Plugin</h1>
-          <Text style={{fontSize: 1.5 + "rem", marginBottom: "10px"}}>A lenti gombra kattintva, elkészül az aktuális állapotból egy
+    <div className="release-wrapper">
+      <h1 className="development-release"><span>Development</span> Release</h1>
+      <div className="release-block">
+        <div className="card-release">
+          <h1>Balasys Website</h1>
+          <Text style={{marginBottom: "10px"}}>A lenti gombra kattintva, újraindul a belső szerver és kikerülnek rá a frissített tartalmak.</Text>
+          <div style={{textAlign: "center"}} className="dev-button">
+            <Button
+              icon={<FontAwesomeIcon icon={faUpload}/>}
+              onClick={() => triggerPublish()}
+            >
+              Development
+            </Button>
+          </div>
+        </div>
+        <div className="card-release">
+          <h1>Balasys Blackowl</h1>
+          <Text style={{marginBottom: "10px"}}>A lenti gombra kattintva, újraindul a belső szerver és kikerülnek rá a frissített tartalmak.</Text>
+          <div style={{textAlign: "center"}} className="dev-button2">
+            <Button
+              icon={<FontAwesomeIcon icon={faUpload}/>}
+              onClick={() => triggerPublishBlackowl()}
+            >
+              Development
+            </Button>
+          </div>
+        </div>
+      </div>
+      <h1 className="live-release"><span>Live</span> Release</h1>
+      <div className="release-block">
+        <div className="card-release">
+          <h1>Balasys Website</h1>
+          <Text style={{marginBottom: "10px"}}>A lenti gombra kattintva, elkészül az aktuális állapotból egy
             production build és kikerül az élő weboldalra.</Text>
-          <Text style={{fontSize: 1.5 + "rem", color: "red", marginBottom: "20px"}}>Figyelem! Release előtt mindig ellenőrizd a development
+          <Text style={{color: "red", marginBottom: "20px"}}>Figyelem! Release előtt mindig ellenőrizd a development
             oldalt és győződj meg arról, hogy minden OK.</Text>
           <div style={{textAlign: "center"}}>
             <Button
               color="success"
-              icon={<FontAwesomeIcon icon={faUpload} />}
+              icon={<FontAwesomeIcon icon={faUpload}/>}
               onClick={() => triggerPublish()}
             >
-              Release
+              Balasys.hu Release
             </Button>
           </div>
         </div>
-        <div style={{
-          justifySelf: "stretch",
-          width: 100 + "%",
-          padding: "20px 20px 20px 20px",
-          boxShadow: "0 2px 4px 0 #e3e9f3",
-          borderRadius: "3px",
-          background: "white"
-        }}>
-          <h1 style={{margin: "0 0 20px 0"}}>Release állapot:</h1>
-          <h2>Teszt szerver:</h2>
-          <img src="https://teamcity.balasys/app/rest/builds/buildType:BalaSysWebsite_Builds_WebsiteGeneration_Develop/statusIcon" alt="teszt" style={{width: "106px", height: "20px"}} />
-          <h2>Élő site:</h2>
-          <img src="https://teamcity.balasys/app/rest/builds/buildType:BalaSysWebsite_Builds_WebsiteGenerationMaster/statusIcon" alt="balasys.hu" style={{width: "106px", height: "20px"}} />
+        <div className="card-release">
+          <h1>Balasys Blackowl</h1>
+          <Text style={{marginBottom: "10px"}}>A lenti gombra kattintva, elkészül az aktuális állapotból egy
+            production build és kikerül az élő weboldalra.</Text>
+          <Text style={{color: "red", marginBottom: "20px"}}>Figyelem! Release előtt mindig ellenőrizd a development
+            oldalt és győződj meg arról, hogy minden OK.</Text>
+          <div style={{textAlign: "center"}}>
+            <Button
+              color="primary"
+              icon={<FontAwesomeIcon icon={faUpload}/>}
+              onClick={() => triggerPublishBlackowl()}
+            >
+              Blackowl Release
+            </Button>
+          </div>
         </div>
-        <div/>
       </div>
-    </>
+      <h1 className="live-release">Állapot</h1>
+      <div className="release-block">
+        <div className="card-release">
+          <h1>Balasys Website</h1>
+          <h2>Teszt szerver:</h2>
+          <img
+            src="https://teamcity.balasys/app/rest/builds/buildType:BalaSysWebsite_Builds_WebsiteGeneration_Develop/statusIcon"
+            alt="teszt" className="build-state"/>
+          <h2>Élő site:</h2>
+          <img
+            src="https://teamcity.balasys/app/rest/builds/buildType:BalaSysWebsite_Builds_WebsiteGenerationMaster/statusIcon"
+            alt="balasys.hu" className="build-state"/>
+        </div>
+        <div className="card-release">
+          <h1>Balasys Blackowl</h1>
+          <h2>Teszt szerver:</h2>
+          <img
+            src="https://teamcity.balasys/app/rest/builds/buildType:BalaSysWebsite_Builds_WebsiteGeneration_Develop/statusIcon"
+            alt="teszt" className="build-state"/>
+          <h2>Élő site:</h2>
+          <img
+            src="https://teamcity.balasys/app/rest/builds/buildType:BalaSysWebsite_Builds_WebsiteGenerationMaster/statusIcon"
+            alt="balasys.hu" className="build-state"/>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- plugins/release-plugin/controllers/release-plugin.js
+++ plugins/release-plugin/controllers/release-plugin.js
@@ -20,4 +20,21 @@ module.exports = {
 
     ctx.send({success});
   },
+
+  publishBlackowl: async (ctx) => {
+    const token = process.env.BEARER_TOKEN;
+    let success;
+
+    const result = await axios({
+      method: 'post',
+      url: 'http://strapi_nginx/teamcity/ajax.html?add2Queue=BalaSysWebsite_Builds_WebsiteGenerationMaster&validate=true&redirectTo=asdasd',
+      headers: {
+        Authorization: "Bearer " + token
+      }
+    }).then(function (response) {
+      success = response.status;
+    });
+
+    ctx.send({success});
+  },
 };
